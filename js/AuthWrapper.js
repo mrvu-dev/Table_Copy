86084180608e9b23317e2d7b4aef523a6af2e097
mrvu.dev <mrvu.dev@gmail.com>
2025-03-23 15:03:29 +0700

    add all file

---
+++ js/AuthWrapper.js
@@ -0,0 +1,53 @@
+class AuthWrapper {
+  constructor() {
+    this.googleAuthToken_ = null;
+    this.googleAuthRefreshToken_ = null;
+
+    this.hasIdentityPerms_ = true;
+  }
+
+  requestPerms() {
+    if (this.hasIdentityPerms_) {
+      return Promise.resolve(true);
+    }
+    return new Promise((resolve, reject) => {
+      chrome.permissions.request(
+        {
+          permissions: ["identity"],
+        },
+        (granted) => {
+          if (chrome.runtime.lastError) {
+            return reject(chrome.runtime.lastError);
+          }
+          if (granted) {
+            this.hasIdentityPerms_ = true;
+            resolve(true);
+          } else {
+            reject(new Error("Chrome Identity permissions not granted."));
+          }
+        }
+      );
+    });
+  }
+
+  getGoogleToken() {
+    if (!this.hasIdentityPerms_) {
+      return this.requestPerms().then(() => this.getGoogleToken());
+    }
+
+    return new Promise((resolve, reject) => {
+      chrome.identity.getAuthToken({ interactive: true }, (token) => {
+        if (chrome.runtime.lastError) {
+          return reject(chrome.runtime.lastError);
+        }
+
+        if (token) {
+          this.googleAuthToken_ = token;
+          resolve({ token });
+        } else {
+          reject(new Error("Unable to retrieve auth token."));
+        }
+      });
+    });
+  }
+}
